b28f86315a755ab65f1454d614a60cf4c723d645
Alexandra Romano <alex.romano80@gmail.com>
2022-11-21 14:07:21 -0700

    commit tree aac51a91c1bdbbed98ec65fd5cded18dc8b4b38f

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats =[
+    "Milo", "Otis", "Garfield"
+];
+
+function destructivelyAppendCat(name) { 
+    cats.push(name);
+}
+destructivelyAppendCat;
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+destructivelyPrependCat;
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+ 
+
+function appendCat(name) {
+//     const copyOfCats = cats.slice();
+// copyOfCats.push(name);
+// return copyOfCats;
+const copyOfCats = [...cats, name];
+return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0,2);
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1,3);
+    return copyOfCats;
+}
